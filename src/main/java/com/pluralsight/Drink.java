7f7cbbca7ccdd02ec86ed58c2c5c676be0a391bd
Alexander Diaz <adiaz1@my.yearupunited.org>
2025-11-14 01:50:25 -0800

    Added Drink class with size pricing

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -1,4 +1,27 @@
 package com.pluralsight;
 
 public class Drink {
+        private String size;
+        private String flavor;
+
+        public Drink(String size, String flavor) {
+            this.size = size;
+            this.flavor = flavor;
+        }
+
+        public double getPrice() {
+            return switch (size) {
+                case "1" -> 2.00;
+                case "2" -> 2.50;
+                case "3" -> 3.00;
+                default -> 0;
+            };
+        }
+
+        @Override
+        public String toString() {
+            return flavor + " (" + size + ") - $" + getPrice();
+        }
+    }
+
 }
