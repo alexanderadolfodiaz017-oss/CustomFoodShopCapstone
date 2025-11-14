93bebeeb16bd3356e4c6678ef1f00792113c42ea
Alexander Diaz <adiaz1@my.yearupunited.org>
2025-11-14 02:10:47 -0800

    Added Topping class with premium/regular price

--- src/main/java/com/pluralsight/Topping.java
+++ src/main/java/com/pluralsight/Topping.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
 public class Topping {
+
+        private String name;
+        private boolean premium;
+        private boolean extra;
+
+        public Topping(String name, boolean premium, boolean extra) {
+            this.name = name;
+            this.premium = premium;
+            this.extra = extra;
+        }
+
+        public double getPrice(String size) {
+            if (!premium) return 0;
+
+            return switch (size) {
+                case "1" -> 1.00;
+                case "2" -> 2.00;
+                case "3" -> 3.00;
+                default -> 0;
+            };
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
 }
