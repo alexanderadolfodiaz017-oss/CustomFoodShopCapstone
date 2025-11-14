4a1f49248bd08af608bb2b77afaae0e28f378f1c
Alexander Diaz <adiaz1@my.yearupunited.org>
2025-11-14 01:58:26 -0800

    Added My Side clas

--- src/main/java/com/pluralsight/Side.java
+++ src/main/java/com/pluralsight/Side.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
 public class Side {
+    private String name;
+
+    public Side(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return 1.50;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - $" + getPrice();
+    }
 }
