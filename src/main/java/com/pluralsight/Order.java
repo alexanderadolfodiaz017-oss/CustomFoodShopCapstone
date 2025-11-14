2ce4c34753a75876e7b912e101c4aadf03762c6f
Alexander Diaz <adiaz1@my.yearupunited.org>
2025-11-14 01:46:41 -0800

    Added Order class for managing cart items and totals

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -1,4 +1,43 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+
+    public class Order {
+        private List<Object> items = new ArrayList<>();
+
+        public void addItem(Object item) {
+            items.add(item);
+        }
+
+        public List<Object> getItems() {
+            return items;
+        }
+
+        public double getTotal() {
+            double total = 0;
+
+            for (Object item : items) {
+                if (item instanceof Burger b) total += b.getPrice();
+                else if (item instanceof Drink d) total += d.getPrice();
+                else if (item instanceof Side s) total += s.getPrice();
+            }
+
+            return total;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+
+            for (Object item : items)
+                sb.append(item.toString()).append("\n");
+
+            sb.append("\nTOTAL: $").append(String.format("%.2f", getTotal()));
+            return sb.toString();
+        }
+    }
+
 }
